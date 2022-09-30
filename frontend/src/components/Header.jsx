d3fddb0ac03332fdf75fe7bf0323e3b8b4ab4c9e
Maximiliano Palavecino <maxipp042@gmail.com>
2022-09-29 22:30:34 -0300

    fixed navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,15 +7,21 @@ const Header = () => {
             <div className='title'>
                 <h1>MAX[i]</h1>
             </div>
-            <nav className='navbar'>
-                <ul>
-                    <li><a href='home'>Inicio</a></li>
-                    <li><a href='about'>Sobre mi</a></li>
-                    <li><a href='contact'>Contacto</a></li>
-                </ul>
-            </nav>
-            <div className='background-nav'></div>
-            <div className='background-nav2'></div>
+            <div className='nav'>
+                <div className='background-nav'></div>
+                <div className='background-nav2'></div>
+                <nav className='navbar'>
+                        <ul>
+                            <li><a href='home'>Inicio</a></li>
+                            <li><a href='about'>Sobre mi</a></li>
+                            <li><a href='contact'>Contacto</a></li>
+                        </ul>
+                </nav>
+            </div>
+            
+
+            
+            
         </div>
     );
 }
